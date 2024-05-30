b1bb031d2cc7f0725299bf50e920f184b41642a3
nirgluzman <nir.gluzman@gmail.com>
2024-05-30 18:15:52 +0200

    add a basic local notification

--- App.js
+++ App.js
@@ -1,11 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Button, View } from 'react-native';
+
+import * as Notifications from 'expo-notifications';
+
+// config how notifications should be handled by the device.
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false
+  })
+});
 
 export default function App() {
+  async function scheduledNotificationHandler() {
+    // schedule a notification to be triggered in the future.
+    await Notifications.scheduleNotificationAsync({
+      content: {
+        // notification content
+        title: 'My first local notification!',
+        body: 'This is the body of the notification!',
+        data: {
+          // Data associated with the notification, not displayed.
+          data: 'This is some data!'
+        }
+      },
+      trigger: {
+        // when to trigger the notification.
+        seconds: 5 // number of seconds until this notification is triggered.
+      }
+    });
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style='auto' />
+      <Button title='Schedule Notification' onPress={scheduledNotificationHandler} />
     </View>
   );
 }
@@ -15,6 +45,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
+    justifyContent: 'center'
+  }
 });
