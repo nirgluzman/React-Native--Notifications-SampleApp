10207ef7b3e8d277a492c7a1afbe385fb45f38a4
nirgluzman <nir.gluzman@gmail.com>
2024-05-30 22:22:06 +0200

    add request permission for notifications

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import { useEffect } from 'react';
 
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Button, View } from 'react-native';
+import { StyleSheet, Button, View, Alert, Platform } from 'react-native';
 
 import * as Notifications from 'expo-notifications';
+import Constants from 'expo-constants'; // to get the projectId value from the app config.
 
 // config how notifications should be handled by the device.
 Notifications.setNotificationHandler({
@@ -15,7 +16,47 @@ Notifications.setNotificationHandler({
 });
 
 export default function App() {
-  // register an event listner to user tapping on notifications.
+  // fetch the push token from the device.
+  useEffect(() => {
+    async function configurePushNotifications() {
+      // check the current permissions settings related to notifications.
+      const { status } = await Notifications.getPermissionsAsync();
+      let finalStatus = status;
+
+      if (finalStatus !== 'granted') {
+        // request permissions for notifications.
+        const { status } = await Notifications.requestPermissionsAsync();
+        finalStatus = status;
+      }
+
+      if (finalStatus !== 'granted') {
+        Alert.alert('Permission required', 'Push notifications need the appropriate permissions!');
+        return;
+      }
+
+      // get the push token from the device.
+      // this is the Expo token that can be used to send a push notification to the device using Expo's push notifications service.
+      const pushTokenData = await Notifications.getExpoPushTokenAsync({
+        projectId: Constants.expoConfig.extra.eas.projectId
+      });
+
+      console.log(pushTokenData);
+
+      if (Platform.OS === 'android') {
+        // assign given notification channel to a notification channel group.
+        Notifications.setNotificationChannelAsync('default', {
+          name: 'default',
+          importance: Notifications.AndroidImportance.DEFAULT,
+          vibrationPattern: [0, 250, 250, 250],
+          lightColor: '#FF231F7C'
+        });
+      }
+    }
+
+    configurePushNotifications();
+  }, []);
+
+  // register an event listener to user tapping on notifications.
   useEffect(() => {
     // Listeners registered by this method will be called whenever a notification is received while the app is running.
     const subscriptionReceivedListener = Notifications.addNotificationReceivedListener(
@@ -32,6 +73,8 @@ export default function App() {
         console.log(userName);
       });
 
+    // useEffect cleanup function.
+    // it does not only run when our component wants to unmount — it also runs right before the execution of the next scheduled effect.
     return () => {
       subscriptionReceivedListener.remove();
       subscriptionResponseReceivedListener.remove();
